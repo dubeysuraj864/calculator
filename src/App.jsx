d6e48e7f13306da5a8bcdbd6529926e306d7417a
Suraj Dubey <suraj@Surajs-MacBook-Pro.local>
2023-05-03 22:53:46 +0530

    used various functions and buttons to get the value of buttons, used js functions like concat, slice, eval and useState hook.

--- src/App.jsx
+++ src/App.jsx
@@ -1,79 +1,100 @@
+/* eslint-disable no-eval */
+import { useState } from "react";
+
+
 function App() {
+  const[display, setDisplay] = useState("");
+
+  const handleClick = (e) => {
+   setDisplay( display.concat(e.target.name))
+  }
+  const allClear =()=> {
+    setDisplay("")
+  }
+  const backspace = () => {
+    setDisplay(display.slice(0, display.length - 1))
+  }
+
+  const calculate = ()=> {
+
+    setDisplay(eval(display));
+  }
+
   return (
     <div className="App flex justify-center  my-20 h-full text-white">
       <div className="container w-fit md:w-fit h-[90%] m-2 rounded-xl shadow-md bg-gray-400">
-        <div className="output m-2 bg-gray-500 p-4 rounded shadow-md">0</div>
+      <input value={display} onChange={ (e) => setDisplay(e.target.value)} type="text" className="output m-2 w-[93%] flex justify-center items-center bg-gray-500 p-4 rounded shadow-md" />
         <div className="buttons flex flex-col m-2">
           <div className="row1 flex ">
-            <div className="w-10 h-10 flex justify-center items-center bg-gray-500 rounded-full m-2 cursor-pointer hover:shadow-none shadow-md transition-all">
+            <button onClick={allClear} className="w-10 h-10 flex justify-center items-center bg-gray-500 rounded-full m-2 cursor-pointer hover:shadow-none shadow-md transition-all">
               AC
+            </button>
+            <div onClick={backspace} className="w-10 h-10 flex justify-center items-center bg-gray-500 rounded-full m-2 cursor-pointer hover:shadow-none shadow-md transition-all">
+              C
             </div>
-            <div className="w-10 h-10 flex justify-center items-center bg-gray-500 rounded-full m-2 cursor-pointer hover:shadow-none shadow-md transition-all">
-              +/-
-            </div>
-            <div className="w-10 h-10 flex justify-center items-center bg-gray-500 rounded-full m-2 cursor-pointer hover:shadow-none shadow-md transition-all">
+            <button name="%" onClick={handleClick} className="w-10 h-10 flex justify-center items-center bg-gray-500 rounded-full m-2 cursor-pointer hover:shadow-none shadow-md transition-all">
               %
-            </div>
-            <div className="w-10 h-10 flex justify-center items-center bg-orange-500 rounded-full m-2 cursor-pointer hover:shadow-none shadow-md transition-all">
+            </button>
+            <button name="+"  onClick={handleClick} className="w-10 h-10 flex justify-center items-center bg-orange-500 rounded-full m-2 cursor-pointer hover:shadow-none shadow-md transition-all">
               ÷
-            </div>
+            </button>
           </div>
           <div className="row1 flex">
-            <div className="w-10 h-10 flex justify-center items-center bg-gray-500 rounded-full m-2 cursor-pointer hover:shadow-none shadow-md transition-all">
+            <button name="1" onClick={handleClick} className="w-10 h-10 flex justify-center items-center bg-gray-500 rounded-full m-2 cursor-pointer hover:shadow-none shadow-md transition-all">
               1
-            </div>
-            <div className="w-10 h-10 flex justify-center items-center bg-gray-500 rounded-full m-2 cursor-pointer hover:shadow-none shadow-md transition-all">
+            </button>
+            <button name="2" onClick={handleClick} className="w-10 h-10 flex justify-center items-center bg-gray-500 rounded-full m-2 cursor-pointer hover:shadow-none shadow-md transition-all">
               2
-            </div>
-            <div className="w-10 h-10 flex justify-center items-center bg-gray-500 rounded-full m-2 cursor-pointer hover:shadow-none shadow-md transition-all">
+            </button>
+            <button name="3" onClick={handleClick} className="w-10 h-10 flex justify-center items-center bg-gray-500 rounded-full m-2 cursor-pointer hover:shadow-none shadow-md transition-all">
               3
-            </div>
-            <div className="w-10 h-10 flex justify-center items-center bg-orange-500 rounded-full m-2 cursor-pointer hover:shadow-none shadow-md transition-all">
+            </button>
+            <button name="*" onClick={handleClick} className="w-10 h-10 flex justify-center items-center bg-orange-500 rounded-full m-2 cursor-pointer hover:shadow-none shadow-md transition-all">
             ×
 
-            </div>
+            </button>
           </div>
           <div className="row1 flex">
-            <div className="w-10 h-10 flex justify-center items-center bg-gray-500 rounded-full m-2 cursor-pointer hover:shadow-none shadow-md transition-all">
+            <button name="4" onClick={handleClick} className="w-10 h-10 flex justify-center items-center bg-gray-500 rounded-full m-2 cursor-pointer hover:shadow-none shadow-md transition-all">
             4
-            </div>
-            <div className="w-10 h-10 flex justify-center items-center bg-gray-500 rounded-full m-2 cursor-pointer hover:shadow-none shadow-md transition-all">
+            </button>
+            <button name="5" onClick={handleClick} className="w-10 h-10 flex justify-center items-center bg-gray-500 rounded-full m-2 cursor-pointer hover:shadow-none shadow-md transition-all">
               5
-            </div>
-            <div className="w-10 h-10 flex justify-center items-center bg-gray-500 rounded-full m-2 cursor-pointer hover:shadow-none shadow-md transition-all">
+            </button>
+            <button name="6" onClick={handleClick} className="w-10 h-10 flex justify-center items-center bg-gray-500 rounded-full m-2 cursor-pointer hover:shadow-none shadow-md transition-all">
               6
-            </div>
-            <div className="w-10 h-10 flex justify-center items-center bg-orange-500 rounded-full m-2 cursor-pointer hover:shadow-none shadow-md transition-all">
+            </button>
+            <button name="-" onClick={handleClick} className="w-10 h-10 flex justify-center items-center bg-orange-500 rounded-full m-2 cursor-pointer hover:shadow-none shadow-md transition-all">
               -
-            </div>
+            </button>
           </div>
           <div className="row1 flex">
-            <div className="w-10 h-10 flex justify-center items-center bg-gray-500 rounded-full m-2 cursor-pointer hover:shadow-none shadow-md transition-all">
+            <button name="1" onClick={handleClick} className="w-10 h-10 flex justify-center items-center bg-gray-500 rounded-full m-2 cursor-pointer hover:shadow-none shadow-md transition-all">
               1
-            </div>
-            <div className="w-10 h-10 flex justify-center items-center bg-gray-500 rounded-full m-2 cursor-pointer hover:shadow-none shadow-md transition-all">
+            </button>
+            <button name="2" onClick={handleClick} className="w-10 h-10 flex justify-center items-center bg-gray-500 rounded-full m-2 cursor-pointer hover:shadow-none shadow-md transition-all">
               2
-            </div>
-            <div className="w-10 h-10 flex justify-center items-center bg-gray-500 rounded-full m-2 cursor-pointer hover:shadow-none shadow-md transition-all">
+            </button>
+            <button name="3" onClick={handleClick} className="w-10 h-10 flex justify-center items-center bg-gray-500 rounded-full m-2 cursor-pointer hover:shadow-none shadow-md transition-all">
               3
-            </div>
-            <div className="w-10 h-10 flex justify-center items-center bg-orange-500 rounded-full m-2 cursor-pointer hover:shadow-none shadow-md transition-all">
+            </button>
+            <button name="+" onClick={handleClick} className="w-10 h-10 flex justify-center items-center bg-orange-500 rounded-full m-2 cursor-pointer hover:shadow-none shadow-md transition-all">
               +
-            </div>
+            </button>
           </div>
           <div className="row1 flex">
           <div>
-          <div className="w-24 h-10 flex justify-center items-center bg-gray-500 rounded-full m-2 cursor-pointer hover:shadow-none shadow-md transition-all">
+          <button name="0" onClick={handleClick} className="w-24 h-10 flex justify-center items-center bg-gray-500 rounded-full m-2 cursor-pointer hover:shadow-none shadow-md transition-all">
               0
-            </div>
+            </button>
           
           </div>
-            <div className="w-10 h-10 flex justify-center items-center bg-gray-500 rounded-full m-2 cursor-pointer hover:shadow-none shadow-md transition-all">
+            <button name="." onClick={handleClick} className="w-10 h-10 flex justify-center items-center bg-gray-500 rounded-full m-2 cursor-pointer hover:shadow-none shadow-md transition-all">
               .
-            </div>
-            <div className="w-10 h-10 flex justify-center items-center bg-orange-500 rounded-full m-2 cursor-pointer hover:shadow-none shadow-md transition-all">
+            </button>
+            <button onClick={calculate} name="=" className="w-10 h-10 flex justify-center items-center bg-orange-500 rounded-full m-2 cursor-pointer hover:shadow-none shadow-md transition-all">
               =
-            </div>
+            </button>
           </div>
         </div>
       </div>
